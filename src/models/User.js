9ae020617c77e2a143766513bf7fd4a7cf923012
Oscar Calle <gameplaysoscar@gmail.com>
2022-01-22 16:53:07 -0500

    Added user model file

---
+++ src/models/User.js
@@ -0,0 +1,52 @@
+import { Schema, model } from 'mongoose'
+import bcrypt from 'bcryptjs'
+
+const UserSchema = new Schema(
+   {
+      name: {
+         type: String,
+         required: true,
+      },
+      email: {
+         type: String,
+         required: true,
+      },
+      password: {
+         type: String,
+         required: true,
+      },
+      state: {
+         type: Boolean,
+         default: true,
+      },
+      role: {
+         type: String,
+         enum: ['CLIENT', 'ADMIN', 'DELIVERY'],
+      },
+   },
+   {
+      timestamps: true,
+      versionKey: false,
+      toJSON: {
+         getters: true,
+         virtuals: true,
+      },
+      toObject: {
+         getters: true,
+         virtuals: true,
+      },
+   }
+)
+
+UserSchema.statics.encryptPassword = async password => {
+   const salt = await bcrypt.genSalt()
+   const hash = await bcrypt.hash(password, salt)
+   return hash
+}
+
+UserSchema.statics.isValidPassword = async (password, encryptedPassword) => {
+   const compare = await bcrypt.compare(password, encryptedPassword)
+   return compare
+}
+
+export default model('User', UserSchema, 'Users')
