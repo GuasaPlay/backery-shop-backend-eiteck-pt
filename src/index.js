3b40645103f3f36c3104767dd246a4dc4e7408ec
Oscar Calle <gameplaysoscar@gmail.com>
2022-01-22 15:19:29 -0500

    Added the function to create the server with express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('Hola mundo')
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
+import config from './config'
+import DBConnection from './database/DBConnection'
+
+async function startExpressServer() {
+   const app = express()
+   app.use(morgan('dev'))
+   app.use(cors())
+   app.use(express.json())
+
+   app.get('/', (_res, req) => {
+      req.json({ message: 'BACKEND BACKERY SHOP' })
+   })
+
+   await DBConnection()
+
+   app.listen(config.port || 5000, () => {
+      console.log(`🚀 Server ready at http://localhost:${config.port}`)
+   })
+}
+
+startExpressServer()
