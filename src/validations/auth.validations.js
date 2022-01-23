447b92660a3f4986f6a183368c3264f603d4d53a
Oscar Calle <gameplaysoscar@gmail.com>
2022-01-22 20:31:15 -0500

    Added validation for requests

---
+++ src/validations/auth.validations.js
@@ -0,0 +1,22 @@
+import { check } from 'express-validator'
+import validateFields from './checkErrors.validations'
+
+const loginValidations = [
+   check('email')
+      .not()
+      .isEmpty()
+      .withMessage('El correo electrónico es obligatorio')
+      .bail()
+      .isEmail()
+      .withMessage('El correo electrónico no es valido'),
+
+   check('password')
+      .isLength({ min: 6 })
+      .withMessage('La contraseña debe tener al menos 6 caracteres')
+      .isLength({ max: 30 })
+      .withMessage('La contraseña solo puede tener 30 caracteres'),
+
+   validateFields,
+]
+
+export { loginValidations }
